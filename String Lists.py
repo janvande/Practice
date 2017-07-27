8a20da09d9642912275459544655d59d6129fa24
janvande <janvande29@gmail.com>
2017-07-27 15:20:28 +0200

    Klaar met hom

--- String Lists.py
+++ String Lists.py
@@ -1,1 +1,17 @@
-print('Is this working now?')
+yourString = input('Please provide a string, we will check if it is a palindrome - Bob')
+listString = []
+revString = []
+stringLength = len(yourString)
+for char in yourString:
+    listString.append(char)
+for revcar in range(stringLength):
+    revString.append(yourString[((stringLength-1)-revcar)])
+test1 = ''.join(listString)
+test2 = ''.join(revString)
+if test1.upper() == test2.upper():
+    print(str(yourString) + ' - is a palindrome')
+else:
+    revName = ''.join(revString)
+    print(revName + ' - is not a palindrome for - ' + revName)
+
+
